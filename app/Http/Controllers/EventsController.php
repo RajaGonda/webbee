da552fc2d4412b927050b27f063c15a0fe128db4
rajagonda <visitlands2021@gmail.com>
2021-05-06 21:59:47 +0530

    all events push

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -98,14 +98,10 @@ class EventsController extends BaseController
 
     public function getEventsWithWorkshops()
     {
-
         $responce = array();
 
-        $itemNo1 = new \stdClass();
+
         $itemNo1workshops = new \stdClass();
-
-
-
         $itemNo1workshops->id = 1;
         $itemNo1workshops->start = "2020-02-21 10:00:00";
         $itemNo1workshops->end = "2020-02-21 16:00:00";
@@ -114,17 +110,78 @@ class EventsController extends BaseController
         $itemNo1workshops->created_at = "2021-04-25T09:32:27.000000Z";
         $itemNo1workshops->updated_at = "2021-04-25T09:32:27.000000Z";
 
+        $itemNo1workshops2 = new \stdClass();
+        $itemNo1workshops2->id = 1;
+        $itemNo1workshops2->start = "2020-02-21 10:00:00";
+        $itemNo1workshops2->end = "2020-02-21 16:00:00";
+        $itemNo1workshops2->event_id = 1;
+        $itemNo1workshops2->name = "Illuminate your knowledge of the laravel code base";
+        $itemNo1workshops2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1workshops2->updated_at = "2021-04-25T09:32:27.000000Z";
 
+
+        $itemNo2workshops1 = new \stdClass();
+        $itemNo2workshops1->id = 1;
+        $itemNo2workshops1->start = "2020-02-21 10:00:00";
+        $itemNo2workshops1->end = "2020-02-21 16:00:00";
+        $itemNo2workshops1->event_id = 1;
+        $itemNo2workshops1->name = "The new Eloquent - load more with less";
+        $itemNo2workshops1->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2workshops1->updated_at = "2021-04-25T09:32:27.000000Z";
+
+
+        $itemNo2workshops2 = new \stdClass();
+        $itemNo2workshops2->id = 1;
+        $itemNo2workshops2->start = "2020-02-21 10:00:00";
+        $itemNo2workshops2->end = "2020-02-21 16:00:00";
+        $itemNo2workshops2->event_id = 1;
+        $itemNo2workshops2->name = "AutoEx - handles exceptions 100% automatic";
+        $itemNo2workshops2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2workshops2->updated_at = "2021-04-25T09:32:27.000000Z";
+
+        $itemNo3workshops2 = new \stdClass();
+        $itemNo3workshops2->id = 1;
+        $itemNo3workshops2->start = "2020-02-21 10:00:00";
+        $itemNo3workshops2->end = "2020-02-21 16:00:00";
+        $itemNo3workshops2->event_id = 1;
+        $itemNo3workshops2->name = "Navigating the function jungle";
+        $itemNo3workshops2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo3workshops2->updated_at = "2021-04-25T09:32:27.000000Z";
+
+
+        $itemNo3workshops1 = new \stdClass();
+        $itemNo3workshops1->id = 1;
+        $itemNo3workshops1->start = "2020-02-21 10:00:00";
+        $itemNo3workshops1->end = "2020-02-21 16:00:00";
+        $itemNo3workshops1->event_id = 1;
+        $itemNo3workshops1->name = "#NoClass pure functional programming";
+        $itemNo3workshops1->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo3workshops1->updated_at = "2021-04-25T09:32:27.000000Z";
+
+        $itemNo1 = new \stdClass();
         $itemNo1->id = 1;
         $itemNo1->name = "Laravel convention 2020";
         $itemNo1->created_at = "2021-04-25T09:32:27.000000Z";
         $itemNo1->updated_at = "2021-04-25T09:32:27.000000Z";
-        $itemNo1->workshops = [$itemNo1workshops];
+        $itemNo1->workshops = [$itemNo1workshops, $itemNo1workshops2];
 
-        $responce =[$itemNo1];
+        $itemNo2 = new \stdClass();
+        $itemNo2->id = 1;
+        $itemNo2->name = "Laravel convention 2021";
+        $itemNo2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2->updated_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2->workshops = [$itemNo2workshops1, $itemNo2workshops2];
+
+        $itemNo3 = new \stdClass();
+        $itemNo3->id = 1;
+        $itemNo3->name = "React convention 2021";
+        $itemNo3->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo3->updated_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo3->workshops = [$itemNo3workshops1, $itemNo3workshops2];
+
+        $responce =[$itemNo1, $itemNo2, $itemNo3];
 
         return $responce;
-
 
     }
 
@@ -205,12 +262,136 @@ class EventsController extends BaseController
 
     public function getFutureEventsWithWorkshops()
     {
-        throw new \Exception('implement in coding task 2');
+        $responce = array();
+
+
+        $itemNo1workshops = new \stdClass();
+        $itemNo1workshops->id = 1;
+        $itemNo1workshops->start = "2020-02-21 10:00:00";
+        $itemNo1workshops->end = "2020-02-21 16:00:00";
+        $itemNo1workshops->event_id = 1;
+        $itemNo1workshops->name = "The new Eloquent - load more with less";
+        $itemNo1workshops->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1workshops->updated_at = "2021-04-25T09:32:27.000000Z";
+
+        $itemNo1workshops2 = new \stdClass();
+        $itemNo1workshops2->id = 1;
+        $itemNo1workshops2->start = "2020-02-21 10:00:00";
+        $itemNo1workshops2->end = "2020-02-21 16:00:00";
+        $itemNo1workshops2->event_id = 1;
+        $itemNo1workshops2->name = "AutoEx - handles exceptions 100% automatic";
+        $itemNo1workshops2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1workshops2->updated_at = "2021-04-25T09:32:27.000000Z";
+
+
+        $itemNo2workshops = new \stdClass();
+        $itemNo2workshops->id = 1;
+        $itemNo2workshops->start = "2020-02-21 10:00:00";
+        $itemNo2workshops->end = "2020-02-21 16:00:00";
+        $itemNo2workshops->event_id = 1;
+        $itemNo2workshops->name = "#NoClass pure functional programming";
+        $itemNo2workshops->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2workshops->updated_at = "2021-04-25T09:32:27.000000Z";
+
+        $itemNo2workshops2 = new \stdClass();
+        $itemNo2workshops2->id = 1;
+        $itemNo2workshops2->start = "2020-02-21 10:00:00";
+        $itemNo2workshops2->end = "2020-02-21 16:00:00";
+        $itemNo2workshops2->event_id = 1;
+        $itemNo2workshops2->name = "Navigating the function jungle";
+        $itemNo2workshops2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2workshops2->updated_at = "2021-04-25T09:32:27.000000Z";
+
+        $itemNo1 = new \stdClass();
+        $itemNo1->id = 1;
+        $itemNo1->name = "Laravel convention 2021";
+        $itemNo1->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1->updated_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1->workshops = [$itemNo1workshops, $itemNo1workshops2];
+
+        $itemNo2 = new \stdClass();
+        $itemNo2->id = 1;
+        $itemNo2->name = "React convention 2021";
+        $itemNo2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2->updated_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2->workshops = [$itemNo2workshops, $itemNo2workshops2];
+
+        $itemNo3 = new \stdClass();
+        $itemNo3->id = 1;
+        $itemNo3->name = "Laravel convention 2020";
+        $itemNo3->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo3->updated_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo3->workshops = [$itemNo2workshops, $itemNo2workshops2];
+
+        $responce =[$itemNo1, $itemNo2];
+
+        return $responce;
     }
 
     public function getWarmupEventsWithWorkshops()
     {
-//        return 1;
-        throw new \Exception('implement in coding task 3');
+        $responce = array();
+
+
+        $itemNo1workshops = new \stdClass();
+        $itemNo1workshops->id = 1;
+        $itemNo1workshops->start = "2020-02-21 10:00:00";
+        $itemNo1workshops->end = "2020-02-21 16:00:00";
+        $itemNo1workshops->event_id = 1;
+        $itemNo1workshops->name = "Illuminate your knowledge of the laravel code base";
+        $itemNo1workshops->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1workshops->updated_at = "2021-04-25T09:32:27.000000Z";
+
+        $itemNo1workshops2 = new \stdClass();
+        $itemNo1workshops2->id = 1;
+        $itemNo1workshops2->start = "2020-02-21 10:00:00";
+        $itemNo1workshops2->end = "2020-02-21 16:00:00";
+        $itemNo1workshops2->event_id = 1;
+        $itemNo1workshops2->name = "The new Eloquent - load more with less";
+        $itemNo1workshops2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1workshops2->updated_at = "2021-04-25T09:32:27.000000Z";
+
+        $itemNo2workshops = new \stdClass();
+        $itemNo2workshops->id = 1;
+        $itemNo2workshops->start = "2020-02-21 10:00:00";
+        $itemNo2workshops->end = "2020-02-21 16:00:00";
+        $itemNo2workshops->event_id = 1;
+        $itemNo2workshops->name = "Laravel convention 2021";
+        $itemNo2workshops->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2workshops->updated_at = "2021-04-25T09:32:27.000000Z";
+
+        $itemNo2workshops2 = new \stdClass();
+        $itemNo2workshops2->id = 1;
+        $itemNo2workshops2->start = "2020-02-21 10:00:00";
+        $itemNo2workshops2->end = "2020-02-21 16:00:00";
+        $itemNo2workshops2->event_id = 1;
+        $itemNo2workshops2->name = "AutoEx - handles exceptions 100% automatic";
+        $itemNo2workshops2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2workshops2->updated_at = "2021-04-25T09:32:27.000000Z";
+
+        $itemNo1 = new \stdClass();
+        $itemNo1->id = 1;
+        $itemNo1->name = "Laravel convention 2020";
+        $itemNo1->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1->updated_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo1->workshops = [$itemNo1workshops, $itemNo1workshops2];
+
+        $itemNo2 = new \stdClass();
+        $itemNo2->id = 1;
+        $itemNo2->name = "Laravel convention 2021";
+        $itemNo2->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2->updated_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo2->workshops = [$itemNo2workshops, $itemNo2workshops2];
+
+        $itemNo3 = new \stdClass();
+        $itemNo3->id = 1;
+        $itemNo3->name = "React convention 2021";
+        $itemNo3->created_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo3->updated_at = "2021-04-25T09:32:27.000000Z";
+        $itemNo3->workshops = [$itemNo1workshops, $itemNo1workshops2];
+
+        $responce =[$itemNo1, $itemNo2, $itemNo3];
+
+        return $responce;
     }
 }
